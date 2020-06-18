4ba2ddac5baa5d9b85225e2f61f22439abee37d7
ColeConte <colefconte@gmail.com>
2020-06-18 08:33:57 -0700

    added output arg

--- svm.py
+++ svm.py
@@ -6,13 +6,20 @@ svm.py
 import argparse
 import pandas as pd
 
+def svm():
+	return ""
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--kernel")
 parser.add_argument("--dataset")
+parser.add_argument("--cost")
 parser.add_argument("--train")
 parser.add_argument("--test")
+parser.add_argument("--output")
 args = parser.parse_args()
 
 train = pd.read_csv(args.train)
 test = pd.read_csv(args.test)
+f = open(args.output, 'w+')
+f.write(svm())
