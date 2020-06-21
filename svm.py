c5212897040f7215ce2f130d0754ff2261aecf37
ColeConte <colefconte@gmail.com>
2020-06-20 19:05:25 -0700

    mnist dataset

--- svm.py
+++ svm.py
@@ -5,9 +5,26 @@ svm.py
 
 import argparse
 import pandas as pd
+import numpy as np
+import struct
+
+def readIdxFile(x,y):
+	'''https://stackoverflow.com/questions/39969045/parsing-yann-lecuns-mnist-idx-file-format'''
+	with open(x,'rb') as f:
+		magic, size = struct.unpack(">II", f.read(8))
+		nrows, ncols = struct.unpack(">II", f.read(8))
+		xData = np.fromfile(f, dtype=np.dtype(np.uint8).newbyteorder('>'))
+		xData = xData.reshape((size, nrows, ncols))
+		f.close()
+	with open(y,'rb') as f:
+		magic, size = struct.unpack(">II", f.read(8))
+		nrows, ncols = struct.unpack(">II", f.read(8))
+		yData = np.fromfile(f, dtype=np.dtype(np.uint8).newbyteorder('>'))
+		f.close()
+	return xData, yData
 
 def svm():
-	return ""
+	pass
 
 
 parser = argparse.ArgumentParser()
@@ -19,7 +36,7 @@ parser.add_argument("--test")
 parser.add_argument("--output")
 args = parser.parse_args()
 
-train = pd.read_csv(args.train)
-test = pd.read_csv(args.test)
-f = open(args.output, 'w+')
-f.write(svm())
+
+if(args.dataset == "mnist"):
+	trainImgs, trainLbls = readIdxFile("train-images-idx3-ubyte","train-labels-idx1-ubyte")
+	testImgs, testLbls = readIdxFile("t10k-images-idx3-ubyte","t10k-labels-idx1-ubyte")
