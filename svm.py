c164916fcc3d4c9fc4f0ce1c26e383d9137fbf39
ColeConte <colefconte@gmail.com>
2020-06-24 21:45:52 -0700

    Update svm.py
    -fixed Idx read
    -added params
    -set up mnist option

--- svm.py
+++ svm.py
@@ -10,7 +10,9 @@ import struct
 import random
 
 def readIdxFile(x,y):
-	'''https://stackoverflow.com/questions/39969045/parsing-yann-lecuns-mnist-idx-file-format'''
+	'''Referenced:
+	https://stackoverflow.com/questions/39969045/parsing-yann-lecuns-mnist-idx-file-format
+	'''
 	with open(x,'rb') as f:
 		magic, size = struct.unpack(">II", f.read(8))
 		nrows, ncols = struct.unpack(">II", f.read(8))
@@ -19,55 +21,90 @@ def readIdxFile(x,y):
 		f.close()
 	with open(y,'rb') as f:
 		magic, size = struct.unpack(">II", f.read(8))
-		nrows, ncols = struct.unpack(">II", f.read(8))
 		yData = np.fromfile(f, dtype=np.dtype(np.uint8).newbyteorder('>'))
 		f.close()
 	return xData, yData
 
-def trainTestSplit(df):
-	random.seed(13579)
-	print("Splitting data into 80% training, 20% testing.\n")
-	df["trainTest"] = [random.random() for i in range(len(df))]
-	test = df[df["trainTest"]>=0.8]
-	test = test.drop(labels="trainTest",axis=1)
-	train = df[df["trainTest"]<0.8]
-	train = train.drop(labels="trainTest",axis=1)
-	return train,test
-
-def svmTrain(train,cost):
-	'''Train SVM algorithm'''
+def svmTrain(train,lam,T):
+	'''Train SVM algorithm for given training set and cost hyperparameter, and compute
+	training error.'''
+	#random.seed(123)
 	m = len(train)
-	theta = np.zeros(len(train.columns)-1)
-	ws = np.empty([1,len(train.columns)-1])
-	T = 1000
+	theta = pd.DataFrame(0,index=np.arange(1),columns=train.columns[:-1])
+	ws = pd.DataFrame(columns=train.columns[:-1])
 	for t in range(1,T+1):
-		w = np.array([(1.0/(cost*t))*theta])
-		ws = np.append(ws,w,axis=0)
+		w = pd.DataFrame((1.0/(lam*t))*theta)
+		ws = ws.append(w)
 		i = random.randint(0,m-1)
-		if train.iloc[i,-1]*np.dot(w,train.iloc[i,:-1]) < 1:
+		if (train.iloc[i,-1] * w.dot(train.iloc[i,:-1]) < 1).bool():
 			theta = theta + train.iloc[i,-1]*train.iloc[i,:-1]
-	return ws.mean(0)
+	w = ws.mean(0)
+	train["hypothesis"] = train.iloc[:,-1]*train.iloc[:,:-1].dot(w)
+	print(train)
+	train["hypothesis"] = train["hypothesis"].map(lambda x: -1.0 if x>0 else 1.0)
+	errors = float(len(train[train["hypothesis"] != train.iloc[:,-2]]))
+	print("Training error is " + str(errors/len(train)*100) +"%")
+	return w
 
 def svmTest(test,w):
-	'''Test SVM algorithm'''
-	#test["predicted"] = test.map(lambda x: -1 if x.iloc[:,-1]*np.dot(w,x.iloc[:,:-1]) < 0 else 1)
-
+	'''Compute testing error for given test set and weight vector.'''
+	test["hypothesis"] = test.iloc[:,-1]*test.iloc[:,:-1].dot(w)
+	test["hypothesis"] = test["hypothesis"].map(lambda x: -1.0 if x>0 else 1.0)
+	errors = float(len(test[test["hypothesis"] != test.iloc[:,-2]]))
+	print("Testing error is " + str(errors/len(test)*100) +"%")
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--kernel")
 parser.add_argument("--dataset")
 parser.add_argument("--cost")
+parser.add_argument("--lam")
+parser.add_argument("--T")
 parser.add_argument("--output")
 args = parser.parse_args()
 
 
 #TODO: Implement CL interface asking for train/test file paths
+#Default parameters if not included in CL
+if(args.lam == None):
+	lam = 1.0
+else:
+	lam = float(args.lam)
+if(args.T == None):
+	T = 1000
+else:
+	T = int(args.T)
+if (args.output == None):
+	output = "output.txt"
+else:
+	output = args.output
+
+f = open(output, "a")
 if(args.dataset == "mnist"):
 	trainImgs, trainLbls = readIdxFile("train-images-idx3-ubyte","train-labels-idx1-ubyte")
 	testImgs, testLbls = readIdxFile("t10k-images-idx3-ubyte","t10k-labels-idx1-ubyte")
-	#Convert to pandas DFs
-else:
-	data = pd.read_csv('Breast_cancer_data.csv')
-	train,test = trainTestSplit(data)
-	weightVector = svmTrain(train,float(args.cost))
-	svmTest(test,weightVector)
+	train = trainImgs.reshape(trainImgs.shape[0],-1)
+	test = testImgs.reshape(testImgs.shape[0],-1)
+	train = pd.DataFrame(train)
+	test = pd.DataFrame(test)
+	train["y"] = trainLbls
+	test["y"] = testLbls
+	for i in range(10):
+		trainCopy = train.copy()
+		testCopy = test.copy()
+		trainCopy.iloc[:,-1] = trainCopy.iloc[:,-1].map(lambda y: 1 if y == i  else -1)
+		testCopy.iloc[:,-1] = testCopy.iloc[:,-1].map(lambda y: 1 if y == i else -1)
+		weightVector = svmTrain(trainCopy,float(lam),T)
+		svmTest(testCopy,weightVector)
+		with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+			f.write(str(weightVector))
+		
+
+elif(args.dataset == "bcd"):
+	train = pd.read_csv('Breast_cancer_data.csv')
+	#Represent success and failure as -1 and 1
+	train.iloc[:,-1] = train.iloc[:,-1].map(lambda y: -1 if y == 0 else 1)
+	weightVector = svmTrain(train,float(lam),T)
+	with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+		f.write(str(weightVector))
+
+f.close()
